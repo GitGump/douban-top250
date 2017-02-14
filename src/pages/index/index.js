81afa4f1a4a90f0db7caa6fc7ae687857e904524
gitgump <txdsysu@gmail.com>
2017-02-14 18:02:55 +0800

    Some improvements
    1. 增加了加载动画
    2. 修改了列表样式
    3. 增加了异常处理

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -8,23 +8,37 @@ Page({
 		page: 1,
 		count: 10,
 		hasMore: true,
-		title: '',
-		pic: '',
-		rating: ''
+		noMoreData: "没有更多数据了",
+		loadingText: "正在加载中...",
+		isLoading: "false",
+		movieText: {
+			director: "导演：",
+			casts: "主演：",
+			year: "上映年份：",
+			rating: "评分："
+		}
 	},
 
 	onLoad: function() {
 		var that = this;
 		var startIndex = (that.data.page - 1) * that.data.count + 1;
 		var endIndex = that.data.page * that.data.count;
+		that.data.isLoading = true;
 		app.douban.find(app.data.type.top250, that.data.page, that.data.count, function(res) {
 			that.setData({
 				movies: res.data.subjects,
-				page: ++that.data.page
+				page: ++that.data.page,
+				isLoading: !that.data.isLoading
 			});
 			console.warn("Got No. from " + startIndex + " to " + endIndex + " movies, they are: ");
 			console.dir(res.data.subjects);
 			console.warn("So far we've got " + endIndex + " movies totally!");
+		}, function(e) {
+			that.setData({
+				isLoading: !that.data.isLoading
+			});
+			console.error("Error occues as fetching data, see more info: ");
+			console.dir(e);
 		});
 	},
 
@@ -32,7 +46,14 @@ Page({
 		var that = this;
 		var startIndex = (that.data.page - 1) * that.data.count + 1;
 		var endIndex = that.data.page * that.data.count;
+		that.setData({
+			isLoading: true
+		});
 		app.douban.find(app.data.type.top250, that.data.page, that.data.count, function(res) {
+			that.setData({
+				isLoading: !that.data.isLoading
+			});
+
 			if (!res.data.subjects.length) {
 				that.data.hasMore = false;
 				console.warn("No more data!");
@@ -47,6 +68,10 @@ Page({
 			console.dir(res.data.subjects);
 			console.warn("So far we've got " + endIndex + " movies totally!");
 		}, function(res) {
+			that.setData({
+				isLoading: !that.data.isLoading
+			});
+
 			console.error("Error occurs as fetching data!")
 		});
 	},
